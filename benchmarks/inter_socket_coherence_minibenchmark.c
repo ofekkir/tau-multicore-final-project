6e1ede50a2fed440c614f65f0b8f689db9d090cc
noakolikant <noa.kolikant@gmail.com>
2017-07-21 15:22:52 +0300

    Set each thread's affinity

--- benchmarks/inter_socket_coherence_minibenchmark.c
+++ benchmarks/inter_socket_coherence_minibenchmark.c
@@ -1,10 +1,14 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sched.h>
 
 typedef struct {
     int local_cycles;
+    cpu_set_t mask;
 } thread_struct;
 
 int shared_var = 0;
@@ -17,6 +21,8 @@ void *myThreadFun(void *vargp)
     thread_struct * ptr = (thread_struct *)vargp;
     int local_cycles = ptr->local_cycles;
 
+    sched_setaffinity(0, sizeof(ptr->mask), &ptr->mask);
+
     while(1)
     {
         for(i = 0; i < local_cycles; i++)
@@ -24,7 +30,8 @@ void *myThreadFun(void *vargp)
             local_var++;
         }
         shared_var++;
-//        printf("shared_var %d \n", shared_var);
+//      printf("shared_var %d \n", shared_var);
+
     }
     return NULL;
 }
@@ -35,6 +42,20 @@ int main()
     thread_struct ts1, ts2;
     ts1.local_cycles = 1000;
     ts2.local_cycles = 1000;
+    CPU_ZERO(&ts1.mask);
+    CPU_ZERO(&ts2.mask);
+
+    CPU_SET(0, &ts1.mask);
+    CPU_SET(0, &ts2.mask);
+
+    pthread_create(&t1, NULL, myThreadFun, &ts1);
+    pthread_create(&t2, NULL, myThreadFun, &ts2);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    exit(0);
+}
 
     pthread_create(&t1, NULL, myThreadFun, &ts1);
     pthread_create(&t2, NULL, myThreadFun, &ts2);
