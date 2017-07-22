e5c92c354cf1bc2a6491895ed230ddaca880cf7d
ofek <ofekkir@gmail.com>
2017-07-22 12:21:02 +0300

    add documentation about performance counters

--- config.py
+++ config.py
@@ -7,14 +7,23 @@ REMOTE_MEMORY_ACCESS_THRESHOLD_PER_TASK = 1
 PERF_EVENTS = \
     [
         # Intra socket coherence
+        # Retired load uops with L2 cache hits as data sources.
         'mem_load_uops_retired_l2_miss',
+        # Retired load uops whose data source is LLC miss.
         'mem_load_uops_retired_l3_miss',
+        # Retired load uops whose data source was LLC hit with no snoop required.
         'mem_load_uops_retired_l3_hit',
+
         # Remote DRAM
+        # Retired load uops whose data source was remote DRAM.
         'mem_load_uops_l3_miss_retired_remote_dram',
+
         # Inter socket coherence
+        # Retired load uops whose data source was remote HITM.
         'mem_load_uops_l3_miss_retired_remote_hitm',
+        # Retired load uops whose data source was forwards from a remote cache.
         'mem_load_uops_l3_miss_retired_remote_fwd',
+
         # Memory bandwidth
         'LLC-misses',
 
