97eeeb8781a710f631ea83fc8200e6c7cbac7bdc
ofek <ofekkir@gmail.com>
2017-07-28 11:15:00 +0300

    added prints for experminet results - on each socket, which core runs which tasks

--- remapper.py
+++ remapper.py
@@ -5,6 +5,7 @@ import subprocess
 import pprint
 
 import config
+moved_inter_socket = []
 
 class Remapper(object):
     def __init__(self, hw, classifications):
@@ -12,11 +13,24 @@ class Remapper(object):
         self._hw = hw
         self._classifications = classifications
 
+        self.sock_0 = {}
+        for i in self._hw[0]:
+            self.sock_0[i] = 0
+
+        self.sock_1 = {}
+        for i in self._hw[1]:
+            self.sock_1[i] = 0
+
+
         blacklist = [os.getpid()] # Ignoring sam process
         self._processes = self._init_processes(blacklist)
 
-    @staticmethod
-    def _init_processes(blacklist):
+        print(self.sock_0)
+        print(self.sock_1)
+
+
+
+    def _init_processes(self, blacklist):
         processes = []
         pids = [int(pid) for pid in subprocess.check_output('ps -eTotid='.split()).splitlines()]
 
@@ -41,13 +55,20 @@ class Remapper(object):
             except psutil.NoSuchProcess:
                 continue
 
+
+            if process.name() == 'group_A' or process.name() == 'group_B':
+                if process.cpu_affinity()[0] in self._hw[1]:
+                    self.sock_1[process.cpu_affinity()[0]] = process.name()
+                else:
+                    self.sock_0[process.cpu_affinity()[0]] = process.name()
+
             processes.append(process)
 
         # pprint.pprint(processes)
 
         return processes
 
-    def move(self, src_list, dst_list):
+    def move(self, src_list, dst_list, i):
         src_core = src_list[0]
         dst_core = dst_list[0]
 
@@ -55,14 +76,19 @@ class Remapper(object):
 
         src_list.remove(src_core)
 
-        print('moving src_core={},dst_core={}'.format(src_core, dst_core))
+        # print('moving src_core={},dst_core={}'.format(src_core, dst_core))
 
         for task in src_tasks:
-            print('moving task {}'.format(task))
+            # print('moving task {}'.format(task))
             task.cpu_affinity([dst_core])
+            if task.name() == 'group_A' or task.name() == 'group_B':
+                if i == 0:
+                    self.sock_0[dst_core] = task.name()
+                else:
+                    self.sock_1[dst_core] = task.name()
 
 
-    def swap(self, src_list, dst_list):
+    def swap(self, src_list, dst_list, i):
         src_core = src_list[0]
         dst_core = dst_list[0]
 
@@ -72,18 +98,32 @@ class Remapper(object):
         src_list.remove(src_core)
         dst_list.remove(dst_core)
 
-        print('swapping. src={},,dst_core={}'.format(src_core, dst_core))
+        # print('swapping. src={},,dst_core={}'.format(src_core, dst_core))
 
         for task in src_tasks:
-            print('swapping task {} from src'.format(task))
+            # print('swapping task {} from src'.format(task))
             task.cpu_affinity([dst_core])
+            if task.name() == 'group_A' or task.name() == 'group_B':
+                if i == 0:
+                    self.sock_0[dst_core] = task.name()
+                else:
+                    self.sock_1[dst_core] = task.name()
+
+
 
         for task in dst_tasks:
-            print('swapping task {} from dst'.format(task))
+            # print('swapping task {} from dst'.format(task))
             task.cpu_affinity([src_core])
+            if task.name() == 'group_A' or task.name() == 'group_B':
+                if i == 0:
+                    self.sock_1[src_core] = task.name()
+                else:
+                    self.sock_0[src_core] = task.name()
 
 
     def _remap_inter_socket_coherence(self):
+        moved_inter_socket.clear()
+
         for socket_i in self._hw:
             if not self._classifications[socket_i].is_inter:
                 continue
@@ -92,34 +132,38 @@ class Remapper(object):
                 if socket_i == socket_j:
                     continue
 
-                print('socket i = {}, socket j = {}'.format(socket_i, socket_j))
+                # print('socket i = {}, socket j = {}'.format(socket_i, socket_j))
 
                 if len(self._classifications[socket_i].is_inter + self._classifications[socket_i].is_intra) < \
                         len(self._hw[socket_i]) \
                         and self._classifications[socket_j].is_inter:
 
                     while self._classifications[socket_i].is_idle and self._classifications[socket_j].is_inter:
-                        print('idle-inter')
+                        # print('idle-inter')
                         self.move(self._classifications[socket_j].is_inter,
-                                  self._classifications[socket_i].is_idle)
+                                  self._classifications[socket_i].is_idle, socket_i)
 
-                    print('finished idle-inter')
+                    # print('finished idle-inter')
 
                     while self._classifications[socket_i].is_cpu_bound and self._classifications[socket_j].is_inter:
-                        print('cpu-inter')
+                        # print('cpu-inter')
                         self.swap(self._classifications[socket_j].is_inter,
-                                  self._classifications[socket_i].is_cpu_bound)
+                                  self._classifications[socket_i].is_cpu_bound, socket_i)
 
-                    print('finished cpu-inter')
+                    # print('finished cpu-inter')
 
                     while self._classifications[socket_i].is_memory_bound and self._classifications[socket_j].is_inter:
-                        print('memory-inter')
+                        # print('memory-inter')
                         self.swap(self._classifications[socket_j].is_inter,
-                                  self._classifications[socket_i].is_memory_bound)
+                                  self._classifications[socket_i].is_memory_bound, socket_i)
 
-                    print('finished memory-inter')
+                    # print('finished memory-inter')
 
-                print('socket j is inter = {}'.format(self._classifications[socket_j].is_inter))
+                # print('socket j is inter = {}'.format(self._classifications[socket_j].is_inter))
+                print('\n---------------------')
+                print(self.sock_0)
+                print(self.sock_1)
+                print('---------------------\n')
 
     def remap_processes(self):
         self._remap_inter_socket_coherence()
